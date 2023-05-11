53f262b5960c269e0363af89c6d3865a12685d3a
Nicole Hawkey <n.laggner@gmx.at>
2023-05-11 14:23:31 +0700

    changing api-key name

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: process.env.API_KEY
+    apiKey: process.env.CLARIFAI_API_KEY
   }); 
 
 const handleApiCall = (req, res) => {
